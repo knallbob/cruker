4972b7c3bea5166e48a328129c595331bbfe1089
Pascal Stoever <pascal.stoever@tu-dresden.de>
2026-03-19 14:03:18 +0000

    basic boot loader

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,72 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
+
+use core::arch::asm;
+
+// ///////////////////////////////////
+// / RUST MACROS
+// ///////////////////////////////////
+#[macro_export]
+macro_rules! print
+{
+	($($args:tt)+) => ({
+
+	});
+}
+#[macro_export]
+macro_rules! println
+{
+	() => ({
+		print!("\r\n")
+	});
+	($fmt:expr) => ({
+		print!(concat!($fmt, "\r\n"))
+	});
+	($fmt:expr, $($args:tt)+) => ({
+		print!(concat!($fmt, "\r\n"), $($args)+)
+	});
+}
+
+// ///////////////////////////////////
+// / LANGUAGE STRUCTURES / FUNCTIONS
+// ///////////////////////////////////
+#[unsafe(no_mangle)]
+extern "C" fn eh_personality() {}
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+	print!("Aborting: ");
+	if let Some(p) = info.location() {
+		println!(
+					"line {}, file {}: {}",
+					p.line(),
+					p.file(),
+					info.message().unwrap()
+		);
+	}
+	else {
+		println!("no information available.");
+	}
+	abort();
+}
+#[unsafe(no_mangle)]
+extern "C"
+fn abort() -> ! {
+	loop {
+		unsafe {
+            // The asm! syntax has changed in Rust.
+            // For the old, you can use llvm_asm!, but the
+            // new syntax kicks ass--when we actually get to use it.
+			asm!("wfi");
+		}
+	}
+}
+
+
+#[unsafe(no_mangle)]
+extern "C"
+fn kmain() {
+	// Main should initialize all sub-systems and get
+	// ready to start scheduling. The last thing this
+	// should do is start the timer.
+}
