274180c458186f345178b32c3f3d77a503ec207f
knallbob <pascal.stoever@protonmail.com>
2026-03-20 10:29:26 +0000

    added basic example code

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,56 @@
 #![no_std]
 #![no_main]
-#![feature(panic_info_message)]
 
-use core::arch::asm;
+extern crate alloc;
 
-// ///////////////////////////////////
-// / RUST MACROS
-// ///////////////////////////////////
-#[macro_export]
-macro_rules! print
-{
-	($($args:tt)+) => ({
+use alloc::format;
+use buddy_system_allocator::LockedHeap;
+use core::arch::global_asm;
+use core::panic::PanicInfo;
+use core::ptr;
 
-	});
+const HEAPSIZE: usize = 65536;
+
+#[global_allocator]
+static HEAP_ALLOCATOR: LockedHeap<32> = LockedHeap::<32>::new();
+
+static mut HEAP: [u8; HEAPSIZE] = [0; HEAPSIZE];
+
+global_asm!(include_str!("asm/boot.S"));
+
+fn uart_print(message: &str) {
+    const UART: *mut u8 = 0x10000000 as *mut u8;
+
+    for c in message.bytes() {
+        unsafe {
+            ptr::write_volatile(UART, c);
+        }
+    }
 }
-#[macro_export]
-macro_rules! println
-{
-	() => ({
-		print!("\r\n")
-	});
-	($fmt:expr) => ({
-		print!(concat!($fmt, "\r\n"))
-	});
-	($fmt:expr, $($args:tt)+) => ({
-		print!(concat!($fmt, "\r\n"), $($args)+)
-	});
-}
 
-// ///////////////////////////////////
-// / LANGUAGE STRUCTURES / FUNCTIONS
-// ///////////////////////////////////
+#[allow(static_mut_refs)] // HEAP.as_mut_ptr() creates a mutable reference to mutable static
 #[unsafe(no_mangle)]
-extern "C" fn eh_personality() {}
+pub extern "C" fn kmain() {
+    uart_print("Hello, world!\n");
+
+    // SAFETY: `HEAP` is only used here and `entry` is only called once.
+    unsafe {
+        // Give the allocator some memory to allocate.
+        HEAP_ALLOCATOR
+            .lock()
+            .init(HEAP.as_mut_ptr() as usize, HEAPSIZE);
+    }
+
+    // Now we can do things that require heap allocation.
+    for ctr in 1..10 {
+        let message = format!("Ticks: {ctr}\n");
+        let temp_str = message.as_str();
+        uart_print(temp_str);
+    }
+}
+
 #[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-	print!("Aborting: ");
-	if let Some(p) = info.location() {
-		println!(
-					"line {}, file {}: {}",
-					p.line(),
-					p.file(),
-					info.message().unwrap()
-		);
-	}
-	else {
-		println!("no information available.");
-	}
-	abort();
-}
-#[unsafe(no_mangle)]
-extern "C"
-fn abort() -> ! {
-	loop {
-		unsafe {
-            // The asm! syntax has changed in Rust.
-            // For the old, you can use llvm_asm!, but the
-            // new syntax kicks ass--when we actually get to use it.
-			asm!("wfi");
-		}
-	}
-}
-
-
-#[unsafe(no_mangle)]
-extern "C"
-fn kmain() {
-	// Main should initialize all sub-systems and get
-	// ready to start scheduling. The last thing this
-	// should do is start the timer.
+fn panic(_info: &PanicInfo) -> ! {
+    uart_print("Something went wrong.\n");
+    loop {}
 }
